7fa9768569405fa6efee5e67b2498e68c17b0faa
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-06-15 10:11:58 +0200

    relationship_mapping refactor

--- lib/api_mapper/relationship_mapping.rb
+++ lib/api_mapper/relationship_mapping.rb
@@ -1,38 +1,15 @@
 module ApiMapper
-  class RelationshipMapping
+  class RelationshipMapping < AttributeMapping
     Error = Class.new(StandardError)
 
-    attr_reader :from, :to
-
     def initialize(from, to, mapping)
-      @from, @to, @mapping = from, to, mapping
-    end
-
-    def match?(key)
-      parent_from == key
-    end
-
-    def parent_from
-      from.to_s.split('.').first
-    end
-
-    def extract(*args)
-      origin = MappingOrigin.new(*args)
-
-      keys.inject(origin.value) { |val, msg| val.fetch(msg) } if match?(origin.key)
-    rescue KeyError => exception
-      raise Error.new(exception.message)
-    end
-
-    def keys
-      keys = from.to_s.split('.')
-      keys.shift
-      keys
+      super(from, to)
+      @mapping = mapping
     end
 
     def extract_mapped(*args)
-      key = args[0]
-      @mapping.map(extract_hash(*args)) if match?(key)
+      origin = MappingOrigin.new(*args)
+      @mapping.map(extract_hash(*args)) if match?(origin.key)
     end
 
     alias_method :extract_hash, :extract
