786974589a78457b6cad5947dacc61e08f6b91e4
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-06-15 12:35:53 +0200

    add collection tests

--- test/api_mapper/collection_test.rb
+++ test/api_mapper/collection_test.rb
@@ -3,27 +3,65 @@ require 'test_helper'
 module ApiMapper
   class CollectionTest < Minitest::Test
 
-    def mappings
-      mappings = ApiMapper::Collection.new
-      mappings.add id_mapping
-      mappings.add name_mapping
-      mappings
+    def test_add_returns_self
+      adding_result = collection.add(Object.new)
+
+      assert_equal collection, adding_result
     end
 
-    def id_mapping
-      ApiMapper::AttributeMapping.new("id")
+    def test_add_item
+      item = Object.new
+
+      assert_equal 0, collection.count
+      collection.add item
+      assert_equal 1, collection.count
+      assert_includes collection, item
     end
 
-    def name_mapping
-      ApiMapper::AttributeMapping.new("myName", "name")
+    def test_iterating
+      item = Minitest::Mock.new
+      item.expect(:mark, true)
+      collection.add item
+
+      collection.each { |i| i.mark }
+
+      item.verify
     end
 
-    def test_find_for_implicit
-      assert_equal id_mapping.to, mappings.match("id").first.to
+    def test_match
+      item1 = item_maching
+      item2 = item_maching
+
+      collection.add item1
+      collection.add item2
+
+      collection.add item_not_matching
+
+      assert_equal [item1, item2], collection.match(:foo)
     end
 
-    def test_find_for_explicit
-      assert name_mapping.to, mappings.match("myName").first.to
+    private
+
+    class Item
+      def initialize(key)
+        @key = key
+      end
+
+      def match?(key)
+        @key == key
+      end
+    end
+
+    def item_maching
+      Item.new(:foo)
+    end
+
+    def item_not_matching
+      Item.new(:bar)
+    end
+
+    def collection
+      @collection ||= ApiMapper::Collection.new
     end
   end
 
