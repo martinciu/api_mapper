af009422f77b25c4b0c7a3a29c8205bb3985fb1e
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-06-15 19:33:50 +0200

    group requires

--- lib/api_mapper.rb
+++ lib/api_mapper.rb
@@ -5,18 +5,21 @@ require 'faraday'
 require 'json'
 require 'addressable/template'
 
+require "api_mapper/version"
+
 require "api_mapper/collection"
-require "api_mapper/error"
 require "api_mapper/index_path"
 
-require "api_mapper/version"
+require "api_mapper/error"
+require "api_mapper/source_element"
+
 require "api_mapper/attribute_mapping"
 require "api_mapper/relationship_mapping"
-require "api_mapper/response"
-require "api_mapper/response_descriptor"
 require "api_mapper/object_manager"
 require "api_mapper/object_mapping"
-require "api_mapper/source_element"
+require "api_mapper/response_descriptor"
+
+require "api_mapper/response"
 
 module ApiMapper
   # Your code goes here...
