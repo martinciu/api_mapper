b8d74329900c872b04ac8f312d65c7908a62239c
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2015-08-13 23:35:49 +0200

    Documentation for Router class

--- lib/api_mapper/router.rb
+++ lib/api_mapper/router.rb
@@ -1,27 +1,71 @@
 module ApiMapper
   class Router
     class << self
+      ##
+      # Defines GET route
+      #
+      # @param (String) path URI template based on RFC 6570 (http://tools.ietf.org/html/rfc6570)
+      # @param (Object) mapper mapper for the route
+      #
+      # @example
+      #   get 'users/{id}', UserMapper.new
       def get(path, mapper)
         add_route(:get, path, mapper)
       end
 
+      ##
+      # Defines PATCH route
+      #
+      # @param (String) path URI template based on RFC 6570 (http://tools.ietf.org/html/rfc6570)
+      # @param (Object) mapper mapper for the route
+      #
+      # @example
+      #   post 'user', UserMapper.new
       def patch(path, mapper)
         add_route(:patch, path, mapper)
       end
 
+      ##
+      # Defines POST route
+      #
+      # @param (String) path URI template based on RFC 6570 (http://tools.ietf.org/html/rfc6570)
+      # @param (Object) mapper mapper for the route
+      #
+      # @example
+      #   patch 'user', UserMapper.new
       def post(path, mapper)
         add_route(:post, path, mapper)
       end
 
-      def add_route(method, mapper, path)
-        routes << Route.new(method, mapper, path)
-      end
-
+      ##
+      # List of defined routes
+      #
+      # @return (Array[Route]) defined routes
+      #
+      # @api private
       def routes
         @routes ||= []
       end
+
+      private
+
+      def add_route(method, mapper, path)
+        routes << Route.new(method, mapper, path)
+      end
     end
 
+    ##
+    # Finds a defined mapper for given HTTP method and path
+    #
+    # @param (Symbol) method HTTP method one of :get, :patch, :post
+    # @param (String) path request path
+    #
+    # @return (Object) mapper defined in the router
+    #
+    # @example
+    #   router.resolve(:get, '/users/123')
+    #
+    # @api private
     def resolve(method, path)
       self.class.routes.find { |route| route.match(method, path) }
     end
